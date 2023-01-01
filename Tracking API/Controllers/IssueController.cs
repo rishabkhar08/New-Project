cae654ff37d0563a76f064957e08561a931fcc1f
Rishab Khar <kharrishab@gmail.com>
2023-01-01 16:30:03 +0530

    Add project files.

---
+++ Tracking API/Controllers/IssueController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Tracking_API.Data;
+using Tracking_API.Models;
+
+namespace Tracking_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IssueController : ControllerBase
+    {
+        private readonly IssueDBContext _context;
+
+        public IssueController(IssueDBContext context) => _context = context;
+
+        [HttpGet]
+        public async Task<IEnumerable<Issue>> Get ()
+        
+            => await _context.Issues.ToListAsync();
+
+        [HttpGet("Id")]
+        [ProducesResponseType(typeof(Issue), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var issue = await _context.Issues.FindAsync(id);
+            return issue == null ? NotFound() : Ok(issue);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(Issue issue)
+        {
+            await _context.Issues.AddAsync(issue);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetById), new { id = issue.Id }, issue);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, Issue issue) 
+        {
+            if (id != issue.Id) return BadRequest();
+            _context.Entry(issue).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType (StatusCodes.Status204NoContent)]
+        [ProducesResponseType (StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var issue = await _context.Issues.FindAsync(id);
+            if(issue == null) return NotFound();
+
+            _context.Issues.Remove(issue);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+
+    }
+}
---
+++ Tracking API/Data/IssueDBContext.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using Tracking_API.Models;
+
+namespace Tracking_API.Data
+{
+    public class IssueDBContext: DbContext
+    {
+        public IssueDBContext(DbContextOptions<IssueDBContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<Issue> Issues { get; set; }
+    }
+}
